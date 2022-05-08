5afe1977c0a84d0b65eec3f4dc9fbed2b1262283
Danny Whittaker <zallist@gmail.com>
2022-05-08 20:30:22 +0100

    Initial commit with code

---
+++ Plugins/UniversalShaderGraphExtensions/Editor/UniversalTargetExtensions/AssetCallbacks/CreateSimpleLitShaderGraph.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEditor.ShaderGraph;
+using UnityEngine.Rendering;
+
+namespace UnityEditor.Rendering.Universal.ShaderGraph
+{
+    static class CreateSimpleLitShaderGraph
+    {
+        [MenuItem("Assets/Create/Shader Graph/URP/Simple Lit Shader Graph", priority = CoreUtils.Priorities.assetsCreateShaderMenuPriority)]
+        public static void CreateSimpleLitGraph()
+        {
+            var target = (UniversalTarget)Activator.CreateInstance(typeof(UniversalTarget));
+            target.TrySetActiveSubTarget(typeof(UniversalSimpleLitSubTarget));
+
+            var blockDescriptors = new[]
+            {
+                BlockFields.VertexDescription.Position,
+                BlockFields.VertexDescription.Normal,
+                BlockFields.VertexDescription.Tangent,
+                BlockFields.SurfaceDescription.BaseColor,
+                BlockFields.SurfaceDescription.NormalTS,
+                //BlockFields.SurfaceDescription.Metallic,
+                BlockFields.SurfaceDescription.Smoothness,
+                BlockFields.SurfaceDescription.Emission,
+                //BlockFields.SurfaceDescription.Occlusion,
+            };
+
+            GraphUtil.CreateNewGraphWithOutputs(new[] { target }, blockDescriptors);
+        }
+    }
+}
